a571e512663eb8a14958fe793ca8106cc4b24b98
Robert Yepez <robertyepez0208@hotmail.com>
2025-06-25 07:21:25 +0200

    feat: Remove abilities placeholder and update AbilityService with HttpClient integration

--- src/app/abilities/shared/ability-service.ts
+++ src/app/abilities/shared/ability-service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Ability } from './ability';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AbilityService {
-
+  private http = inject(HttpClient);
+  private _abilities = signal<Ability>
   constructor() { }
 }
